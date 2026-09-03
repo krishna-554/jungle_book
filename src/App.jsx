8dcf773c802937ad4607acb3356b6abb65bdcbea
sredha <sredhamanoj07@gmail.com>
2026-09-04 00:43:35 +0530

    Set up Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-orange-50 flex items-center justify-center">
+      <h1 className="text-5xl font-black text-orange-500">
+        🐾 JUNGLE BOOK
+      </h1>
+    </div>
+  )
+}
+
+export default App
